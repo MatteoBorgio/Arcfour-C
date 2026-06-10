cb0faf35a797b73534919e827e58741e340502af
matteoborgio <matteo6.borgio@gmail.com>
2026-06-10 12:07:29 +0200

    add function printbin and correct errors in arcfour.h and example.c

--- arcfour.h
+++ arcfour.h
@@ -1,18 +1,21 @@
+#include <assert.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
-#define rc4_decrypt(x, y) rc4_encrypt(x, y);
+#define rc4_decrypt(x, y) rc4_encrypt(x, y)
+
+typedef struct {
+    // ...
+    int x;
+} Arcfour;
 
 typedef unsigned char int8;
 typedef unsigned short int int16;
 typedef unsigned int int32;
 
-struct arcfour {
-    // ...
-};
-
 Arcfour *rc4_init(int8 *key, int16 size);
 int8 rc4_byte(void);
-int8 *rc4_encrypt(int *source, int16 size);
+int8 *rc4_encrypt(int8 *source, int16 size);
--- example.c
+++ example.c
@@ -1,17 +1,31 @@
 #include "arcfour.h"
 
-int main(void);
+void printbin(int8 *text, const int16 size) {
+    int16 i;
+    int8 *p;
 
-int main() {
+    assert(size > 0);
+
+    for (i = size, p = text; i; i--, p++) {
+        if (!((i + 1) % 2)) {
+            printf(" ");
+        }
+        printf("%.02x", *p);
+    }
+    printf("\n");
+    return;
+}
+
+int main(void) {
     Arcfour *rc4;
     int16 size_key, size_text;
-    char *key, *source, *encrypted, *decrypted;
+    int8 *key, *source, *encrypted;
 
-    key = "8fA2bX9vE1mQ7zP4kL6wT9jR3cH5nG1p";
-    size_key = strlen(key);
-    source = "Example. Verification code: 481516. Is the Arcfour algorithm "
-             "functioning correctly?";
-    size_text = strlen(source);
+    key = (int8 *)"8fA2bX9vE1mQ7zP4kL6wT9jR3cH5nG1p";
+    size_key = strlen((char *)key);
+    source = (int8 *)"Example. Verification code: 481516. Is the Arcfour "
+                     "algorithm functioning correctly?";
+    size_text = strlen((char *)source);
 
     printf("Initializing encryption...\n");
     rc4 = rc4_init(key, size_key);
@@ -19,4 +33,10 @@ int main() {
 
     printf("'%s'\n ->", source);
     encrypted = rc4_encrypt(source, size_text);
+
+    if (encrypted != NULL) {
+        printbin(encrypted, size_text);
+    }
+
+    return 0;
 }
